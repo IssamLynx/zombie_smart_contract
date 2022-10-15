02fde423e7f782cd9b7db92c96b1390d611b35e2
IssamLynx <hm_bakha@esi.dz>
2022-10-15 17:50:41 +0200

    Resolved lesson 2 chapter 2 3 cryptozombies tutorial

--- Contract.sol
+++ Contract.sol
@@ -14,9 +14,14 @@ contract ZombieFactory {
 
     Zombie[] public zombies;
 
+    mapping (uint => address) public zombieToOwner;
+    mapping (address => uint) ownerZombieCount;
+
     function _createZombie(string _name, uint _dna) private {
-        uint id =zombies.push(Zombie(_name, _dna))-1;
-        NewZombie(id,_name,_dna);
+        uint id = zombies.push(Zombie(_name, _dna)) - 1;
+        zombieToOwner[id] = msg.sender;
+        ownerZombieCount[msg.sender]++;
+        NewZombie(id, _name, _dna);
     }
 
     function _generateRandomDna(string _str) private view returns (uint) {
